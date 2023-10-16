f8f377dd0788effee4a1be2300bb1b60480b07df
Kwan Suk Kim <linupa@gmail.com>
2023-10-15 23:39:26 -0700

    Modify downloadBook
    Download only new books

--- downloadBook.py
+++ downloadBook.py
@@ -13,68 +13,35 @@ connection = Config['connection'].format(password)
 def updateDB(currDb, srcDb, dstDb, dbName, dbKey, log = False):
     updates = compare(srcDb, currDb, log=log)
     updateSQL(updates, srcDb, dstDb, dbName, dbKey)
-
-def downloadBook(clib, db):
-    print("="*80)
-    print("Book")
-    books = convertToSQL(db.book, "BARCODE", sqlBookDict)
-    updateDB(clib.books, books, clib, "book", "BARCODE")
-
-    print("="*80)
-    print("MARC")
-    marcs = convertToSQL(db.marc, "SEQ", sqlMARCDict)
-    updateDB(clib.marcs, marcs, clib, "marc", "SEQ")
-
+    return updates[0]
 
 def downloadDatabase(clib, db):
-    print("Download database")
-
-#    for key in books:
-#        book = books[key]
-#        if "MOD_DATE" not in book or len(book["MOD_DATE"]) == 0:
-#            print(book)
-#            book["MOD_DATE"] = book["REG_DATE"]
-#            print(books[key])
+    print("Download book database")
 
     print("="*80)
     print("Book")
     books = convertToSQL(db.book, "BARCODE", sqlBookDict)
-    updateDB(clib.books, books, clib, "book", "BARCODE")
+    newBookList = updateDB(clib.books, books, clib, "book", "BARCODE")
+
+    newBooks = dict()
+    print(newBookList)
+    for bookId in newBookList:
+        newBooks[bookId] = books[bookId]
 
     print("="*80)
     print("MARC")
     marcs = convertToSQL(db.marc, "SEQ", sqlMARCDict)
     updateDB(clib.marcs, marcs, clib, "marc", "SEQ")
-
-    print("="*80)
-    print("User")
-    users = convertToSQL(db.user, "USER_CODE", sqlUserDict)
-    for key in users:
-        user = users[key]
-        if 'DELETE_YN' not in user:
-            user['DELETE_YN'] = "N"
-    updateDB(clib.users, users, clib, "users", "USER_CODE")
 
     print("="*80)
     print("Rent")
-    rents = convertToSQL(db.rent, "BARCODE", sqlRentDict)
-    for key in books:
-        if key not in rents:
-            rents[key] = {'SEQ': books[key]['SEQ'], 'BARCODE': key, 'STATS': 0, 'USERS': '', 'LENT_DATE': '', 'RETURN_DATE': '', 'RESERVE_USER': '', 'RESERVE_DATE': '', 'EXTEND_COUNT': 0, 'DELETE_YN': books[key]['DELETE_YN'], 'ATTACH': 'N', 'ATTACH_USER': ''}
-    updateDB(clib.rents, rents, clib, "book_lent", "BARCODE")
+    rents = dict()
+    for bookId in newBooks:
+        rents[bookId] = {'SEQ': books[bookId]['SEQ'], 'BARCODE': bookId, 'STATS': 0, 'USERS': '', 'LENT_DATE': '', 'RETURN_DATE': '', 'RESERVE_USER': '', 'RESERVE_DATE': '', 'EXTEND_COUNT': 0, 'DELETE_YN': books[bookId]['DELETE_YN'], 'ATTACH': 'N', 'ATTACH_USER': ''}
+    for key in rents:
+        print(rents[key])
 
-    print("="*80)
-    print("RentHistory")
-    rentlog = convertToSQL(db.rentLog, "SEQ", sqlRentHistoryDict)
-    for entry in clib.rentHistory:
-        regDate = entry['REG_DATE']
-        if len(regDate) == 18:
-            print(regDate)
-            regDate = regDate[0:11] + "0" + regDate[11:]
-            print(regDate)
-    updateDB(list2dict(clib.rentHistory, "SEQ"), rentlog, clib, "rental_history", "SEQ")
-
-    checkUnique(rentlog, "SEQ")
+    updateSQL([newBookList, list(), list()], rents, clib, "book_lent", "BARCODE")
 
 if __name__ == '__main__':
     # Read SQL
@@ -85,6 +52,5 @@ if __name__ == '__main__':
     print(connection)
     client = MongoClient(connection)
     db = client.library
-    print("Check")
 
     downloadDatabase(clib, db)
