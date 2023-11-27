6f81fa5dcee508e2a4ae6b3fffbde138db4ca435
Kwan Suk Kim <linupa@gmail.com>
2023-11-26 22:21:16 -0800

    Added .gitignore

--- registerTask.ps1
+++ registerTask.ps1
@@ -2,4 +2,4 @@ $a = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"c:\cli
 $t = New-ScheduledTaskTrigger -AtLogon
 $s = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 $p = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
-Register-ScheduledTask -TaskName "WSL2PortsBridge" -Action $a -Trigger $t -Settings $s -Principal $p
+Register-ScheduledTask -TaskName "LibServerPrep" -Action $a -Trigger $t -Settings $s -Principal $p
