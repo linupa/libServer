b9e4dd434d7d2bd18df892480a372e4a97415321
Kwan Suk Kim <linupa@gmail.com>
2023-12-16 07:22:16 -0800

    Added hint for delete scheduled task

--- registerTask.ps1
+++ registerTask.ps1
@@ -3,3 +3,6 @@ $t = New-ScheduledTaskTrigger -AtLogon
 $s = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 $p = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
 Register-ScheduledTask -TaskName "LibServerPrep" -Action $a -Trigger $t -Settings $s -Principal $p
+
+# Delete
+# schtasks /Delete /TN "LibServerPrep"
