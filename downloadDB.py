ceada2f0d6775f442b3efaf1d5e9cceae9696734
Kwan Suk Kim <linupa@gmail.com>
2023-12-17 00:31:56 -0800

    downloadDB only accepts new rent logs
    --force overwrites every rent logs

--- downloadDB.py
+++ downloadDB.py
@@ -10,6 +10,8 @@ import tkinter as tk
 import threading
 from tkinter import ttk
 import time
+import sys
+import argparse
 
 import dns.resolver
 dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
@@ -18,33 +20,9 @@ dns.resolver.default_resolver.nameservers=['8.8.8.8']
 password = Config['password']
 connection = Config['connection'].format(password)
 
-global bookCount
-global bookProgress
-global marcCount
-global marcProgress
-global userCount
-global userProgress
-global rentCount
-global rentProgress
-global rentLogCount
-global rentLogProgress
 global shutdown
 
-def updateDB(currDb, srcDb, dstDb, dbName, dbKey, callback = None, log = False):
-    updates = compare(srcDb, currDb, log=log)
-    updateSQL(updates, srcDb, dstDb, dbName, dbKey, callback)
-
-def downloadDatabase(clib, db, widgets):
-    global bookCount
-    global bookProgress
-    global marcCount
-    global marcProgress
-    global userCount
-    global userProgress
-    global rentCount
-    global rentProgress
-    global rentLogCount
-    global rentLogProgress
+def downloadDatabase(clib, db, widgets, forced):
     print("Download database")
 
 #    for key in books:
@@ -57,14 +35,12 @@ def downloadDatabase(clib, db, widgets):
     print("="*80)
     bookInfo = db.command("collstats", "book")
     widgets["book"].setCount(bookInfo['count'])
-    bookCount = bookInfo['count']
     print(f"Book ({bookInfo['count']})")
     books = convertToSQL(db.book, "BARCODE", sqlBookDict, widgets["book"].setDownload, 10)
 
     print("="*80)
     marcInfo = db.command("collstats", "marc")
     widgets["marc"].setCount(marcInfo['count'])
-    marcCount = marcInfo['count']
     print(f"MARC ({marcInfo['count']})")
     marcs = convertToSQL(db.marc, "SEQ", sqlMARCDict, widgets["marc"].setDownload, 10)
     matchCount = 0
@@ -117,20 +93,8 @@ def downloadDatabase(clib, db, widgets):
             user['DELETE_YN'] = "N"
 
     print("="*80)
-    rentInfo = db.command("collstats", "rent")
-    widgets["rent"].setCount(rentInfo['count'])
-    print(f"User ({userInfo['count']})")
-    rentCount = rentInfo['count']
-    print(f"Rent ({rentInfo['count']})")
-    rents = convertToSQL(db.rent, "BARCODE", sqlRentDict, widgets["rent"].setDownload, 10)
-    for key in books:
-        if key not in rents:
-            rents[key] = {'SEQ': books[key]['SEQ'], 'BARCODE': key, 'STATS': 0, 'USERS': '', 'LENT_DATE': '', 'RETURN_DATE': '', 'RESERVE_USER': '', 'RESERVE_DATE': '', 'EXTEND_COUNT': 0, 'DELETE_YN': books[key]['DELETE_YN'], 'ATTACH': 'N', 'ATTACH_USER': ''}
-
-    print("="*80)
     rentLogInfo = db.command("collstats", "rentLog")
     widgets["rentHistory"].setCount(rentLogInfo['count'])
-    rentLogCount = rentLogInfo['count']
     print(f"RentHistory ({rentLogInfo['count']})")
     rentlog = convertToSQL(db.rentLog, "SEQ", sqlRentHistoryDict, widgets["rentHistory"].setDownload, 10)
     for entry in clib.rentHistory:
@@ -141,16 +105,50 @@ def downloadDatabase(clib, db, widgets):
             print(regDate)
 
     print("="*80)
+    rentInfo = db.command("collstats", "rent")
+    widgets["rent"].setCount(rentInfo['count'])
+    print(f"User ({userInfo['count']})")
+    print(f"Rent ({rentInfo['count']})")
+    rents = convertToSQL(db.rent, "BARCODE", sqlRentDict, widgets["rent"].setDownload, 10)
+    for key in books:
+        if key not in rents:
+            rents[key] = {'SEQ': books[key]['SEQ'], 'BARCODE': key, 'STATS': 0, 'USERS': '', 'LENT_DATE': '', 'RETURN_DATE': '', 'RESERVE_USER': '', 'RESERVE_DATE': '', 'EXTEND_COUNT': 0, 'DELETE_YN': books[key]['DELETE_YN'], 'ATTACH': 'N', 'ATTACH_USER': ''}
+
+    print("="*80)
     print("Update DBs")
-    updateDB(clib.books, books, clib, "book", "BARCODE", widgets["book"].setUpdate)
-    updateDB(clib.marcs, marcs, clib, "marc", "SEQ", widgets["marc"].setUpdate)
-    updateDB(clib.users, users, clib, "users", "USER_CODE", widgets["user"].setUpdate)
-    updateDB(clib.rents, rents, clib, "book_lent", "BARCODE", widgets["rent"].setUpdate)
-    updateDB(list2dict(clib.rentHistory, "SEQ"), rentlog, clib, "rental_history", "SEQ", widgets["rent"].setUpdate)
+#    updateDB(clib.books, books, clib, "book", "BARCODE", widgets["book"].setUpdate)
+    updates = compare(books, clib.books, False)
+    widgets["book"].setState(f"Add: {len(updates[0])} Changed: {len(updates[1])} Deleted: {len(updates[2])}")
+    updateSQL(updates, books, clib, "book", "BARCODE", widgets["book"].setUpdate)
+
+#    updateDB(clib.marcs, marcs, clib, "marc", "SEQ", widgets["marc"].setUpdate)
+    updates = compare(marcs, clib.marcs, False)
+    widgets["marc"].setState(f"Add: {len(updates[0])} Changed: {len(updates[1])} Deleted: {len(updates[2])}")
+    updateSQL(updates, marcs, clib, "marc", "SEQ", widgets["marc"].setUpdate)
+
+#    updateDB(clib.users, users, clib, "users", "USER_CODE", widgets["user"].setUpdate)
+    updates = compare(users, clib.users, False)
+    widgets["user"].setState(f"Add: {len(updates[0])} Changed: {len(updates[1])} Deleted: {len(updates[2])}")
+    updateSQL(updates, users, clib, "users", "USER_CODE", widgets["user"].setUpdate)
+
+#    updateDB(list2dict(clib.rentHistory, "SEQ"), rentlog, clib, "rental_history", "SEQ", widgets["rent"].setUpdate)
+    currDb = list2dict(clib.rentHistory, "SEQ")
+    updates = compare(rentlog, currDb, False)
+    if not forced and (len(updates[1]) > 0 or len(updates[2]) > 0):
+        widgets["rentHistory"].setState(f"Cannot update rent history")
+        return
+
+    widgets["rentHistory"].setState(f"Add: {len(updates[0])} Changed: {len(updates[1])} Deleted: {len(updates[2])}")
+    updateSQL(updates, rentlog, clib, "rental_history", "SEQ", widgets["rentHistory"].setUpdate)
+
+#    updateDB(clib.rents, rents, clib, "book_lent", "BARCODE", widgets["rent"].setUpdate)
+    updates = compare(rents, clib.rents, False)
+    widgets["rent"].setState(f"Add: {len(updates[0])} Changed: {len(updates[1])} Deleted: {len(updates[2])}")
+    updateSQL(updates, rents, clib, "book_lent", "BARCODE", widgets["rent"].setUpdate)
 
     checkUnique(rentlog, "SEQ")
 
-def downloadThread(window, widgets):
+def downloadThread(window, widgets, forced):
     global shutdown
     # Read SQL
     clib = CLibrary()
@@ -162,7 +160,7 @@ def downloadThread(window, widgets):
     db = client.library
     print("Check")
 
-    downloadDatabase(clib, db, widgets)
+    downloadDatabase(clib, db, widgets, forced)
     print("Done")
     time.sleep(3)
 
@@ -184,6 +182,7 @@ class Progress:
         self.bookProgress = ttk.Progressbar(window, orient="horizontal", mode="determinate", length=600)
         self.bookLabel2 = tk.Label(window, text=name)
         self.bookUpdate = ttk.Progressbar(window, orient="horizontal", mode="determinate", length=600)
+        self.bookState = tk.Label(window, text="")
         self.count = 1
 
     def setCount(self, count):
@@ -196,6 +195,7 @@ class Progress:
     def addUpdate(self, index):
         self.bookLabel2.grid(column = 0, row = index)
         self.bookUpdate.grid(column = 1, row = index)
+        self.bookState.grid(column = 1, row = index+1)
 
     def setDownload(self, value):
         self.bookProgress["value"] = 100 * value / self.count
@@ -203,9 +203,19 @@ class Progress:
     def setUpdate(self, value):
         self.bookUpdate["value"] = value
 
+    def setState(self, state):
+        self.bookState.config(text = state)
+
 if __name__ == '__main__':
     global shutdown
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--force", action="store_true")
+    args = parser.parse_args()
+
+    forced = args.force
+
+    print(forced)
     shutdown = False
     window = tk.Tk()
     window.title("DB download")
@@ -214,13 +224,13 @@ if __name__ == '__main__':
     downloadLabel = tk.Label(window, text="Download Cloud DB")
     updateLabel = tk.Label(window, text="Update CLIB DB")
 
-    items = ["book", "marc", "user", "rent", "rentHistory"]
+    items = ["book", "marc", "user", "rentHistory", "rent"]
     widgets = dict()
     widgets["book"] = Progress(window, "Book")
     widgets["marc"] = Progress(window, "MARC")
     widgets["user"] = Progress(window, "User")
-    widgets["rent"] = Progress(window, "Rent")
     widgets["rentHistory"] = Progress(window, "RentHistory")
+    widgets["rent"] = Progress(window, "Rent")
 
     index = 0
     for i in range(len(items)):
@@ -231,9 +241,9 @@ if __name__ == '__main__':
     index += 1
     for i in range(len(items)):
         widgets[items[i]].addUpdate(index)
-        index += 1
+        index += 2
 
-    thread = threading.Thread(target = downloadThread, args = (window, widgets))
+    thread = threading.Thread(target = downloadThread, args = (window, widgets, forced))
     thread.start()
 
     window.after(1000, timer)
