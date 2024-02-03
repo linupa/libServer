e58ff08e5b935c185c21ee1c4bc04fb88fef18c7
Kwan Suk Kim <linupa@gmail.com>
2024-02-02 23:22:59 -0800

    Organized checkDB

--- checkDB.py
+++ checkDB.py
@@ -31,49 +31,61 @@ def checkDB(mongoDb):
     rentLogs = mdb2dict(mongoDb.rentLog)
     print(f"{len(rentLogs)} rentLogs")
 
-    print("="*80)
-    print("Check DB")
     numDeleted = 0
     stateHist = dict()
     seqNums = set()
+    errorCount = 0
+    print("="*80)
+    print("Compare Book and MARC")
     for key in books:
         book = books[key]
         seqnum = book["seqnum"]
         if seqnum in seqNums:
             print(f"Duplicated seqNum {seqnum}")
+            errorCount += 1
         seqNums.add(seqnum)
         if seqnum not in marcs:
-            print(f"MARC for {key} is missing")
+            print(f"MARC for seq {key} is missing")
             print(book)
+            errorCount += 1
         if book['deleted'].lower() != marcs[seqnum]['DELETE_YN'].lower():
             print(f"Delete flag does not match in book and marc")
             print(book)
             print(marcs[seqnum])
+            errorCount += 1
         if book['deleted'].lower() == "y":
             numDeleted += 1
             if key in rents:
                 print(f"Deleted book has a state")
                 print(book)
                 print(rents[key])
+                errorCount += 1
         else:
             if key[0:2] != "HK":
                 print(f"Invalid ID {key}")
+                errorCount += 1
         if seqnum in rents:
             state = rents[seqnum]['state']
 #            print(f"{key}: {state}")
             stateHist[state] = stateHist[state] + 1 if state in stateHist else 1
 
+    print("="*80)
+    print("Check Sequence numbers")
     print("Check MARC")
     for seqnum in marcs:
         if seqnum not in seqNums:
-            print(f"Seq {seqnum} does not exist")
+            print(f"Seq {seqnum} in MARC does not exist")
+            errorCount += 1
 
     print("Check rents")
     for seqnum in rents:
         if seqnum not in seqNums:
-            print(f"Seq {seqnum} does not exist")
+            print(f"Seq {seqnum} in rent does not exist")
+            errorCount += 1
 
 
+    print("="*80)
+    print("Check MARC data")
     booksFromMarc = dict()
     matchCount = 0
     mismatchCount = 0
@@ -88,6 +100,7 @@ def checkDB(mongoDb):
             newMarc = marc.encode()
             if bytes(orgMarc, "UTF-8") != bytes(newMarc, "UTF-8"):
                 mismatchCount += 1
+                errorCount += 1
             else:
                 matchCount += 1
             book = marc.getBookInfo()
@@ -104,9 +117,11 @@ def checkDB(mongoDb):
             print(orgMarc)
             print(book)
             failCount += 1
-    print(f"Same {matchCount} / Change {mismatchCount} / Fail {failCount} / Total {len(marcs)} // AuthorCode {authorMismatch}")
+            errorCount += 1
+    print(f"Same {matchCount} / Change {mismatchCount} / Fail {failCount} / Total {len(marcs)} // AuthorCode mismatch {authorMismatch}")
     booksFromMarc = convertToMDB(booksFromMarc, "_id", sqlBookDict)
 
+    print("="*80)
     print("Compare Book and MARC")
     for key in booksFromMarc:
         marcBook = booksFromMarc[key]
@@ -116,15 +131,16 @@ def checkDB(mongoDb):
                 print(f"Book {key} mismatch with MARC")
                 print(mdbBook)
                 print(marcBook)
+                errorCount += 1
 
     numValid = len(books) - numDeleted
     numAvail = numValid
     for state in stateHist:
         numAvail -= stateHist[state]
 
-    print(f"Avaiable {numAvail} / Valld {numValid} / All {len(books)} / Deleted {numDeleted}")
-    print(stateHist)
 
+    print("="*80)
+    print("Check rent history")
     keyMap = {"idx": "_id", "book": "book_id", "state": "book_state", "user": "user_id", "date": "timestamp", "retDate": "return_date"}
     noReturn = checkRentHistory(dict2list(rentLogs), keyMap)
     count = 0
@@ -135,23 +151,30 @@ def checkDB(mongoDb):
         if seqnum not in rents:
             print(f"Book {entry} is not in rent list")
             count += 1
+            errorCount += 1
         elif userId != rents[seqnum]['user_id']:
             print(f"{entry} Renter {userId} is different from {rents[seqnum]['user_id']}")
             count += 1
+            errorCount += 1
 
     if count > 0:
         print(f"Mismatch cound {count}")
 
+    print(f"Total error count {errorCount}")
+    print("="*80)
+    print(f"Avaiable {numAvail} / Valld {numValid} / All {len(books)} / Deleted {numDeleted}")
+    print(stateHist)
+
+    return errorCount
+
 if __name__ == '__main__':
     # Open MongoDB
-    print(os.environ)
     if "GITHUB_ACTIONS" in os.environ:
         password = os.environ["MONGODB_PASSWORD"]
     else:
         from config import Config
         password = Config['password']
     connection = 'mongodb+srv://linupa:{}@hkmcclibrary.s59ur1w.mongodb.net/?retryWrites=true&w=majority'.format(password)
-    print(connection)
     client = MongoClient(connection)
     mongoDb = client.library
 
